3e14cf0b37a80fe7035f44c195ff6a44869b74f8
Angel Orozco <orozcoding@gmail.com>
2022-03-21 21:28:48 -0400

    Improve keys for table

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-id INT,
-name TEXT,
+id INT GENERATED ALWAYS AS IDENTITY,
+name VARCHAR(20),
 date_of_birth DATE,
 escape_attempts INT,
 neutered BOOLEAN,
