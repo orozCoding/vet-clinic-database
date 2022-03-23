941ff9dc57b733a4b6998ca01678b511f4a7776e
Angel Orozco <orozcoding@gmail.com>
2022-03-23 00:08:02 -0400

    Added data to new tables
    Added data to the new tables and updated animals table.

--- data.sql
+++ data.sql
@@ -14,3 +14,90 @@ VALUES ('Charmander', 'Feb 8, 2020', -11.00, false, 0),
 ('Boarmon', 'Jun 7, 2005', 20.40, true, 7),
 ('Blossom', 'Oct 13, 1998', 17.00, true, 3),
 ('Ditto', 'May 14, 2022', 22.00, true, 4);
+
+        
+BEGIN;
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+SELECT * FROM owners;
+
+COMMIT;
+
+BEGIN;
+
+INSERT INTO species (name)
+VALUES
+('Pokemon'),
+('Digimon');
+
+SELECT * FROM species;
+
+COMMIT;
+
+BEGIN;
+
+UPDATE animals
+SET species_id = 2
+WHERE name LIKE '%mon';
+
+SELECT * FROM animals;
+
+UPDATE animals
+SET species_id = 1
+WHERE species_id IS NULL;
+
+SELECT * FROM animals;
+
+
+COMMIT;
+
+BEGIN;
+
+UPDATE animals
+SET owner_id = 1
+WHERE name = 'Agumon';
+
+SELECT * FROM animals;
+
+SAVEPOINT SP1;
+
+UPDATE animals
+SET owner_id = 2
+WHERE (name = 'Gabumon')
+OR (name = 'Pikachu');
+
+SELECT * FROM animals;
+
+SAVEPOINT SP2;
+
+UPDATE animals
+SET owner_id = 3
+WHERE (name = 'Devimon')
+OR (name = 'Plantmon');
+
+SELECT * FROM animals;
+
+
+SAVEPOINT SP3;
+
+UPDATE animals
+SET owner_id = 4
+WHERE (name = 'Charmander')
+OR (name = 'Squirtle')
+OR (name = 'Blossom');
+
+SAVEPOINT SP4;
+
+UPDATE animals
+SET owner_id = 5
+WHERE (name = 'Angemon')
+OR (name = 'Boarmon');
+
+SELECT * FROM animals;
